4c92a27e189516695419d4b9179839c5c9b23fbf
Jarek Lipski <loomchild@loomchild.net>
2020-05-23 15:36:32 +0200

    remove fallbacks in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,9 +12,4 @@ export default {
 </script>
 
 <style scoped>
-.theme--dark.v-application {
-  background-color: var(--v-background-base, #121212) !important;
-}
-.theme--light.v-application {
-  background-color: var(--v-background-base, white) !important;
-}</style>
+</style>
